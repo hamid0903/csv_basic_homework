434ce344c9556e6fffb1eba5dde9ec360f019cfd
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-24 22:55:36 +0500

    Task04-1

--- get_first_column.py
+++ get_first_column.py
@@ -6,6 +6,19 @@ def get_first_column(data):
     Return:
         list: First column.
     """
-    return
+    columns=[]
+    txt=data.split('\n')
+    idx=0
+    while idx<len(txt):
+        columns.append(txt[idx].split(','))
+        idx+=1
+    first_column=[]
+    idx=0
+    while idx<len(columns):
+        first_column.append(columns[idx][0])
+        idx+=1
+    return first_column
+data=open('data.csv').read()
+print(get_first_column(data))
     
 # Read the csv file
